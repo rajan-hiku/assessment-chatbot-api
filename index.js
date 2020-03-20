0740961f6aeaee6a4a685299ea416b0e48ac9851
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-20 00:04:20 -0400

    update variable name

--- index.js
+++ index.js
@@ -13,29 +13,31 @@ app.get('/updateAirtable', async (req, res) => {
     const record = await base(airtableBase).select({
       fields: ['PostalCode']
     }).all()
-    const updatedRecord = []
+
+    const airTableUpdatedRecords = []
 
     await Promise.all(record.map(async ({ id, fields }) => {
       const { PostalCode } = fields
       const resultLatLng = await getLatFromPostalCode(PostalCode)
-      updatedRecord.push({ id, fields: { ...resultLatLng } })
+      airTableUpdatedRecords.push({ id, fields: { ...resultLatLng } })
     }))
-    // console.log(updatedRecord)
-    const batchPromise = []
-    const numberOfPromises = updatedRecord.length % 10
+
+    const batchRequest = []
+    const numberOfPromises = airTableUpdatedRecords.length % 10
+
     for (let i = 0; i < numberOfPromises; i++) {
-      batchPromise.push(new Promise((resolve, reject) => {
+      batchRequest.push(new Promise((resolve, reject) => {
         try {
           const start = i * 10
-          const end = start + 10 > updatedRecord.length ? updatedRecord.length : start + 10
-          resolve(base('CenterDetails').update(updatedRecord.slice(start, end)))
+          const end = start + 10 > airTableUpdatedRecords.length ? airTableUpdatedRecords.length : start + 10
+          resolve(base('CenterDetails').update(airTableUpdatedRecords.slice(start, end)))
         } catch (e) {
           reject(e)
         }
       }))
     }
 
-    const results = await Promise.all(batchPromise)
+    const results = await Promise.all(batchRequest)
     res.send(results)
   } catch (err) { console.error(err); res.sendStatus(404).send(err) }
 })
