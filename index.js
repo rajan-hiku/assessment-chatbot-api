c5bad8697cc4091a0baba0322cea9025d76e5f46
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-20 00:03:21 -0400

    add a cron function

--- index.js
+++ index.js
@@ -6,14 +6,37 @@ const Airtable = require('airtable')
 var base = new Airtable({ apiKey: process.env.AIRTABLE }).base('appZv8bkFustjCUXN')
 const opencage = require('opencage-api-client')
 app.use(express.json()) // for parsing application/json
+const airtableBase = 'CenterDetails'
 
-app.get('/', async (req, res) => {
+app.get('/updateAirtable', async (req, res) => {
   try {
-    const record = await base('CenterDetails').find('recspsA9yRI1k3e5F')
-    const { fields } = record
-    const { PostalCode } = fields
-    const postalCode = await getLatFromPostalCode(PostalCode)
-    res.send(postalCode)
+    const record = await base(airtableBase).select({
+      fields: ['PostalCode']
+    }).all()
+    const updatedRecord = []
+
+    await Promise.all(record.map(async ({ id, fields }) => {
+      const { PostalCode } = fields
+      const resultLatLng = await getLatFromPostalCode(PostalCode)
+      updatedRecord.push({ id, fields: { ...resultLatLng } })
+    }))
+    // console.log(updatedRecord)
+    const batchPromise = []
+    const numberOfPromises = updatedRecord.length % 10
+    for (let i = 0; i < numberOfPromises; i++) {
+      batchPromise.push(new Promise((resolve, reject) => {
+        try {
+          const start = i * 10
+          const end = start + 10 > updatedRecord.length ? updatedRecord.length : start + 10
+          resolve(base('CenterDetails').update(updatedRecord.slice(start, end)))
+        } catch (e) {
+          reject(e)
+        }
+      }))
+    }
+
+    const results = await Promise.all(batchPromise)
+    res.send(results)
   } catch (err) { console.error(err); res.sendStatus(404).send(err) }
 })
 
@@ -22,12 +45,13 @@ app.get('/postalCode', async (req, res) => {
   const userPostalCode = await getLatFromPostalCode(postalCode)
   res.send(userPostalCode)
 })
+
 const getLatFromPostalCode = async (postalCode) => {
   try {
     const { results } = await opencage.geocode({ q: postalCode })
     // geometry defaults to Toronto
     const { geometry } = (Array.isArray(results) && results.length > 0 && results[0]) || { 'lat': 43.6915, 'lng': -79.4307 }
-    return JSON.stringify(geometry)
+    return geometry
   } catch (error) {
     return error
   }
