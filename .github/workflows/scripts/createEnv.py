48f58dcac62fa0f21340d35aa587574471617d85
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-22 19:25:15 -0400

    test new file

--- .github/workflows/scripts/createEnv.py
+++ .github/workflows/scripts/createEnv.py
@@ -7,5 +7,5 @@ out_file = ""
 for key in env_keys:
     out_file += key + "=" + os.environ.get(key) + "\n"
 
-with open("/github/workspace/" + str(os.environ.get(".env")), "w") as text_file:
+with open(".env", "w+") as text_file:
     text_file.write(out_file)
