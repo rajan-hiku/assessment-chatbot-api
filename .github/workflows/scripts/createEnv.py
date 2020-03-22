fd68b295f6cc5eba28466469374614666dd43f36
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-22 19:00:14 -0400

    add py script

---
+++ .github/workflows/scripts/createEnv.py
@@ -0,0 +1,11 @@
+import os
+
+env_keys = list(dict(os.environ).keys())
+
+out_file = ""
+
+for key in env_keys:
+    out_file += key + "=" + os.environ.get(key) + "\n"
+
+with open("/github/workspace/" + str(os.environ.get(".env")), "w") as text_file:
+    text_file.write(out_file)
--- .github/workflows/scripts/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-npm run build && twilio-run deploy --account-sid="$ACCOUNT_SID" --auth-toke="$AUTH_TOKEN" && ta update --schema COVID19BOT1.json
