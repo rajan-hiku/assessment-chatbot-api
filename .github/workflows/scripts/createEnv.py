a74521def81b091f1562dceb1ebb18cc97f00016
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-22 20:18:19 -0400

    change .env

--- .github/workflows/scripts/createEnv.py
+++ .github/workflows/scripts/createEnv.py
@@ -7,5 +7,6 @@ out_file = ""
 for key in env_keys:
     out_file += key + "=" + os.environ.get(key) + "\n"
 
-with open(".env", "w+") as text_file:
+with open(os.path.join('.env'), "w+") as text_file:
     text_file.write(out_file)
+    
