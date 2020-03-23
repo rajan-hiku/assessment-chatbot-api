e07ebcc01f18dfcb7eed50a02a5937d18a65aa6d
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-22 21:07:42 -0400

    try now

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ls
+# ls
 cat .env
-twilio-run deploy --account-sid="$ACCOUNT_SID" --auth-token="$AUTH_TOKEN"
-ta update --schema COVID19BOT1.json --account-sid="$ACCOUNT_SID" --auth-token="$AUTH_TOKEN"
+# twilio-run deploy --account-sid="$ACCOUNT_SID" --auth-token="$AUTH_TOKEN"
+# ta update --schema COVID19BOT1.json --account-sid="$ACCOUNT_SID" --auth-token="$AUTH_TOKEN"
