68647df09d5fc389892e63ccdc7cf5f61d21980a
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-25 18:56:46 -0400

    add get message function

--- twilioFunctions/collectfallback.js
+++ twilioFunctions/collectfallback.js
@@ -1,30 +1,30 @@
-const { airTableBase, messagesTable } = require('../constants')
+// const { airTableBase, messagesTable } = require('../constants')
+const { getTextForFunction } = require('../lib/index')
+const fn_callfallback = async function (context, event, callback) {
+  // airTableBase(messagesTable)
+  // .select({ filterByFormula: 'AND(SEARCH("Collect_Fallback", Name),SEARCH("Both",BotType))' })
+  // .eachPage(function page (records, fetchNextPage) {
+  // This function (`page`) will get called for each page of records.
+  // console.log(records[0].fields)
 
-const fn_callfallback = function (context, event, callback) {
-  let message = ''
-  airTableBase(messagesTable)
-    .select({ filterByFormula: 'AND(SEARCH("Collect_Fallback", Name),SEARCH("Both",BotType))' })
-    .eachPage(function page (records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
-      console.log(records[0].fields)
-      message = records[0].fields.Message
-      const responseObject = {
-        actions: [
-          {
-            say: message
-          },
-          {
-            redirect: `${process.env.ASSESMENT_API}/informationRoute`
-          },
-          {
-            listen: false
-          }
-        ]
+  const message = await getTextForFunction('Collect_Fallback', 'Both', 'English')
+  const responseObject = {
+    actions: [
+      {
+        say: message
+      },
+      {
+        redirect: `${process.env.ASSESMENT_API}/informationRoute`
+      },
+      {
+        listen: false
       }
-      callback(null, responseObject)
-    }, function done (err) {
-      if (err) { console.error(err) }
-    })
+    ]
+  }
+  callback(null, responseObject)
+  // }, function done (err) {
+  // if (err) { console.error(err) }
+  // })
 }
 
 exports.handler = fn_callfallback
