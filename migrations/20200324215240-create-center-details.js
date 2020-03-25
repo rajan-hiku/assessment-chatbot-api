58528d0539912fedc44f602ecf738878fdcde441
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-24 20:46:00 -0400

    fix seed

--- migrations/20200324215240-create-center-details.js
+++ migrations/20200324215240-create-center-details.js
@@ -34,6 +34,14 @@ module.exports = {
       },
       lng: {
         type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     })
   },
