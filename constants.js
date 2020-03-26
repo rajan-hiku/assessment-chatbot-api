4801954cdf72cf18189421d960905f963937e91d
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-03-26 19:11:03 -0400

    use a hack for now

--- constants.js
+++ constants.js
@@ -3,7 +3,6 @@ const centerTable = 'CenterDetails'
 const messagesTable = 'TwilioMessages'
 const Airtable = require('airtable')
 const Sequelize = require('sequelize')
-const path = require('path')
 
 const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASS, {
   database: process.env.MYSQL_DATABASE,
@@ -17,10 +16,39 @@ const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME,
 const airTableBase = new Airtable({ apiKey: process.env.AIRTABLE }).base(
   'appZv8bkFustjCUXN'
 )
-const centerTableDB = db.import(path.resolve(__dirname, './models/centerdetails'))
-const hospitalTableDB = db.import(path.resolve(__dirname, './models/hospitaldetails'))
+const centerTableDB = db.define('HospitalDetails', {
+  HospitalName: Sequelize.DataTypes.STRING,
+  StreetAddress: Sequelize.DataTypes.STRING,
+  City: Sequelize.DataTypes.STRING,
+  Province: Sequelize.DataTypes.STRING,
+  PostalCode: Sequelize.DataTypes.STRING,
+  PhoneNumber: Sequelize.DataTypes.STRING,
+  PID: Sequelize.DataTypes.STRING,
+  lat: Sequelize.DataTypes.FLOAT,
+  lng: Sequelize.DataTypes.FLOAT
+}, {})
 
-const messageTableDB = db.import(path.resolve(__dirname, './models/twiliomessages'))
+const hospitalTableDB = db.define('CenterDetails', {
+  CenterName: Sequelize.DataTypes.STRING,
+  StreetAddress: Sequelize.DataTypes.STRING,
+  City: Sequelize.DataTypes.STRING,
+  Province: Sequelize.DataTypes.STRING,
+  PostalCode: Sequelize.DataTypes.STRING,
+  PhoneNumber: Sequelize.DataTypes.STRING,
+  PID: Sequelize.DataTypes.STRING,
+  lat: Sequelize.DataTypes.FLOAT,
+  lng: Sequelize.DataTypes.FLOAT
+}, {
+  tableName: 'CenterDetails'
+})
+
+const messageTableDB = db.define('TwilioMessages', {
+  Name: Sequelize.DataTypes.STRING,
+  Message: Sequelize.DataTypes.TEXT('long'),
+  BotType: Sequelize.DataTypes.STRING,
+  Language: Sequelize.DataTypes.STRING
+}, {})
+
 module.exports = {
   airTableBase,
   centerTable,
